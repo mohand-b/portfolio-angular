3b058cfd88791d0bfc46b1fef4bc1d033923e567
mohand-b <mohandbdj@gmail.com>
2025-06-02 17:15:27 +0200

    feat: add main containers and base router configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./routes/home/containers/home/home').then(m => m.Home)
+  },
+  {
+    path: 'career',
+    loadComponent: () => import('./routes/career/containers/career/career').then(m => m.Career)
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./routes/contact/containers/contact/contact').then(m => m.Contact)
+  },
+  {
+    path: 'skills',
+    loadComponent: () => import('./routes/skills/containers/skills/skills').then(m => m.Skills)
+  },
+  {
+    path: 'console',
+    loadChildren: () => import('./routes/console/console.routes').then(m => m.consoleRoutes)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
