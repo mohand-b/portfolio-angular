4ae24cff52e821d2c596ee4cd3d0925a9a3293ce
mohand-b <mohandbdj@gmail.com>
2025-06-20 22:30:20 +0200

    feat(directive): add FormFieldStyleDirective for custom height and rounded mat-form-field

--- src/app/routes/console/containers/achievement-create/achievement-create.ts
+++ src/app/routes/console/containers/achievement-create/achievement-create.ts
@@ -3,8 +3,8 @@ import {MatButtonModule} from '@angular/material/button';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
-import {Router} from '@angular/router';
 import {ConsoleFacade} from '../../console.facade';
+import {FormFieldStyleDirective} from '../../../../shared/directives/form-field-style.directive';
 
 @Component({
   selector: 'app-achievement-create',
@@ -12,13 +12,13 @@ import {ConsoleFacade} from '../../console.facade';
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    FormFieldStyleDirective
   ],
   templateUrl: './achievement-create.html',
   styleUrl: './achievement-create.scss'
 })
 export class AchievementCreate {
-  private router = inject(Router);
   private consoleFacade = inject(ConsoleFacade);
 
   private fb = inject(FormBuilder);
