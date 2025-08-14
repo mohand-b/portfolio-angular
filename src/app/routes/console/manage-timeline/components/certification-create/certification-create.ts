cab94d8998f77c102b9c66d8b2b81b4c8e880212
mohand-b <mohandbdj@gmail.com>
2025-08-14 12:03:27 +0200

    feat(console/timeline): add CertificationCreate form integrated with side panel

--- src/app/routes/console/manage-timeline/components/certification-create/certification-create.ts
+++ src/app/routes/console/manage-timeline/components/certification-create/certification-create.ts
@@ -1,12 +1,11 @@
-import {Component, effect, inject, input, output, signal} from '@angular/core';
+import {Component, inject, input, output, signal} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import {CreateCertificationDto} from '../../../../career/state/certification/certification.model';
-import {TimelineItemType} from '../../../../career/state/timeline/timeline.model';
+import {ConsoleFacade} from '../../../console.facade';
 
 
 @Component({
@@ -23,10 +22,9 @@ import {TimelineItemType} from '../../../../career/state/timeline/timeline.model
   styleUrl: './certification-create.scss'
 })
 export class CertificationCreate {
-  resetForm = input(0);
-  certificationSubmit = output<CreateCertificationDto>();
+
+  readonly created = output<void>();
   imagePreview = signal<string | null>(null);
-  submitted = output<void>();
   private fb = inject(FormBuilder);
   certificationForm: FormGroup = this.fb.group({
     certificationName: ['', Validators.required],
@@ -36,31 +34,32 @@ export class CertificationCreate {
     location: ['', Validators.required],
     image: [null as File | null],
   });
-
-  private readonly resetFormEffect = effect(() => {
-    const token = this.resetForm();
-    this.certificationForm.reset({type: TimelineItemType.Job});
-    this.imagePreview.set(null);
-  });
+  private consoleFacade = inject(ConsoleFacade);
 
   onSubmit() {
-    this.certificationForm.markAllAsTouched();
-    if (!this.certificationForm.valid) return;
-
-    this.certificationSubmit.emit(this.certificationForm.value);
-  }
-
-  onFileSelected(event: Event): void {
-    const file = (event.target as HTMLInputElement).files?.[0] || null;
-    this.certificationForm.patchValue({image: file});
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        this.imagePreview.set(reader.result as string);
-      };
-      reader.readAsDataURL(file);
+    if (this.certificationForm.invalid) {
+      this.certificationForm.markAllAsTouched();
+      return;
     }
+
+    this.consoleFacade.addCertification(this.certificationForm.value).subscribe({
+      next: () => {
+        this.created.emit();
+        this.certificationForm.reset();
+        this.imagePreview.set(null);
+      },
+      error: () => this.certificationForm.markAsTouched(),
+    });
   }
 
 
+  onFileSelected(evt: Event) {
+    const input = evt.target as HTMLInputElement;
+    const file = input.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = () => this.imagePreview.set(reader.result as string);
+    reader.readAsDataURL(file);
+  }
 }
--- src/app/routes/console/manage-timeline/containers/manage-timeline/manage-timeline.ts
+++ src/app/routes/console/manage-timeline/containers/manage-timeline/manage-timeline.ts
@@ -1,17 +1,8 @@
-import {Component, effect, inject, signal, WritableSignal} from '@angular/core';
-import {
-  TIMELINE_ITEM_TYPE_META,
-  TimelineItemType,
-  TimelineItemTypeMeta
-} from '../../../../career/state/timeline/timeline.model';
+import {Component, signal} from '@angular/core';
+import {TIMELINE_ITEM_TYPE_META, TimelineItemType} from '../../../../career/state/timeline/timeline.model';
 import {MatTabsModule} from '@angular/material/tabs';
-import {ActivatedRoute, Router} from '@angular/router';
 import {JobCreate} from '../../components/job-create/job-create';
-import {ConsoleFacade} from '../../../console.facade';
-import {CreateJobDto} from '../../../../career/state/job/job.model';
-import {toFormData} from '../../../../../shared/extensions/object.extension';
 import {CertificationCreate} from '../../components/certification-create/certification-create';
-import {CreateCertificationDto} from '../../../../career/state/certification/certification.model';
 import {SidePanel} from '../../../../../shared/components/side-panel/side-panel';
 import {UpperCasePipe} from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
@@ -55,45 +46,7 @@ export class ManageTimeline {
   readonly selectedType = toSignal(this.typeControl.valueChanges, {initialValue: this.typeControl.value});
   readonly TimelineItemType = TimelineItemType;
 
-  readonly timelineItemTypesMeta: TimelineItemTypeMeta[] = Object.values(TIMELINE_ITEM_TYPE_META);
-  readonly timelineItemTypes: TimelineItemType[] = this.timelineItemTypesMeta.map(meta => meta.key);
-  resetForm: WritableSignal<number> = signal(0);
-  selectedTabIndex: WritableSignal<number> = signal(0);
   panelOpen = signal(false);
-  private consoleFacade = inject(ConsoleFacade);
-  private route = inject(ActivatedRoute);
-  private router = inject(Router);
-  private syncTabWithQueryParam = effect(() => {
-    const type = this.route.snapshot.queryParamMap.get('type') as TimelineItemType;
-    const idx = this.timelineItemTypes.indexOf(type);
-    this.selectedTabIndex.set(idx >= 0 ? idx : 0);
-  });
-
-  onTabChange(idx: number) {
-    const type = this.timelineItemTypes[idx];
-    this.router.navigate([], {
-      relativeTo: this.route,
-      queryParams: {type},
-      queryParamsHandling: 'merge',
-    }).then(r => {
-    });
-  }
-
-  onJobSubmit(job: CreateJobDto) {
-    this.consoleFacade.addJob(toFormData(job)).subscribe({
-      next: () => this.resetForm.update(v => v + 1),
-      error: () => {
-      }
-    });
-  }
-
-  onCertificationSubmit(certification: CreateCertificationDto) {
-    this.consoleFacade.addCertification(toFormData(certification)).subscribe({
-      next: () => this.resetForm.update(v => v + 1),
-      error: () => {
-      }
-    })
-  }
 
   openPanel() {
     this.panelOpen.set(true);
