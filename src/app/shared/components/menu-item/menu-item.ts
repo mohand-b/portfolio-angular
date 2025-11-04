b33635cf52846265cf21a967680a687587124913
mohand-b <mohandbdj@gmail.com>
2025-11-04 11:49:09 +0100

    style(console): harmonize header and content layout with console-menu design

--- src/app/shared/components/menu-item/menu-item.ts
+++ src/app/shared/components/menu-item/menu-item.ts
@@ -2,10 +2,11 @@ import {Component, Input} from '@angular/core';
 import {MenuEntry} from '../main-menu/main-menu';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-menu-item',
-  imports: [RouterLink, RouterLinkActive, MatIconModule],
+  imports: [RouterLink, RouterLinkActive, MatIconModule, NgClass],
   templateUrl: './menu-item.html',
   styleUrl: './menu-item.scss'
 })
