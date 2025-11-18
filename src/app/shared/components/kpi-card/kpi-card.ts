cdcb932dd66a03350f42d4da6ee2e2f4aca94925
mohand-b <mohandbdj@gmail.com>
2025-11-18 10:16:47 +0100

    refactor(styles): restructure stylesheets and update typography

--- src/app/shared/components/kpi-card/kpi-card.ts
+++ src/app/shared/components/kpi-card/kpi-card.ts
@@ -1,4 +1,4 @@
-import {Component, input} from '@angular/core';
+import {Component, computed, input} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {hexWithAlpha} from '../../utils/color.utils';
 
@@ -12,6 +12,16 @@ export class KpiCard {
   value = input.required<string | number>();
   icon = input<string>('star');
   color = input<string>('#6366f1');
+  isPercent = input<boolean>(false);
 
   protected readonly hexWithAlpha = hexWithAlpha;
+
+  protected readonly formattedValue = computed(() => {
+    const val = this.value();
+
+    if (typeof val === 'number') {
+      return Math.round(val);
+    }
+    return val || 0;
+  });
 }
