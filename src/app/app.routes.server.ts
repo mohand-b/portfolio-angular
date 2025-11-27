738a39ace043b18ff664c8f3e78c6e0611ac62db
mohand-b <mohandbdj@gmail.com>
2025-11-27 21:39:05 +0100

    fix production deployment configuration

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,15 +2,7 @@ import {RenderMode, ServerRoute} from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'contact',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: 'projects/:id',
-    renderMode: RenderMode.Server
-  },
-  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   }
 ];
